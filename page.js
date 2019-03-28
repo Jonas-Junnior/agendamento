01d1632c7feca5ba5f48ed5854f81c93fe94b1c2
Jonas-Junnior <jonasjunnior@hotmail.com>
2019-03-27 21:55:37 -0300

    Add files via upload

--- page.js
+++ page.js
@@ -10,21 +10,24 @@ function closeNav() {
 }
 
 $(document).ready(function(){
+$("#inputcelular").mask("(99) 99999-9999");
+});
+
+$(document).ready(function(){
+  $("#inputCEP").mask("99999-999");
+  });
+
+$(document).ready(function(){
   $('#form').on('submit', function(){
     $('#inputcelular').removeClass('phone')
     .unmask();
-  })
-})
-
-$(document).ready(function(){
-$("#inputCEP").mask("99999-999");
+  });
 });
 
-
-$(document).ready(function(){
+/*$(document).ready(function(){
   $('#enviar').on('click', function(){
     window.alert("Obrigado por se consultar conosco. Iremos lhe enviar um SMS de confirmação. Você será redirecionado para a pagina inicial. Muito Obrigado!")
 
-    window.location.assign("http://odonto.com.vc/index.html");
+    window.location.replace("http://odonto.com.vc/index.html");
   });
-});
+});*/
